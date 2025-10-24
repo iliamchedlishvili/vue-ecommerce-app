ce1e74e3d1f8f73f743952beb8d09de92468a863
Ilia Mchedlishvili <ilia.mchedlishvili@gmail.com>
2025-10-24 07:19:24 +0400

    new button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <script setup>
+
 </script>
 
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="bg-dark text-white p-2">
-        <span class="navbar-brand ml-2">E-Commerce Application</span>
-      </div>
-    </div>
-    <router-view></router-view>
-  </div>
+  <header>
+     E-Commerce App
+  </header>
+
+  <main>
+    <button>Click</button>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
