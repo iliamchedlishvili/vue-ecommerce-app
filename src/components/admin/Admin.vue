4e5e1d79ddfaf2289fbc1c0b90dd9d79ed79d2a6
ilia mchedlishvili <ilia.mchedlishvili@gmail.com>
2025-10-24 12:03:24 +0400

    ccc

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -3,7 +3,6 @@ import ProductDetails from './products/ProductDetails.vue';
 import ProductsTable from './products/ProductsTable.vue';
 </script>
 <template>
-   <ProductsTable></ProductsTable>
-   <ProductDetails></ProductDetails>
+   <RouterView></RouterView>
 </template>
 <style scoped></style>
---
+++ src/components/admin/users/UsersManage.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+<template>
+    <div>
+        Users Manage Component
+    </div>
+</template>
+<style scoped></style>
