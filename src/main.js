3a3f751605e06e73ad274e8cf7bdbaac296d6759
Ilia Mchedlishvili <ilia.mchedlishvili@gmail.com>
2025-10-28 21:21:38 +0400

    new

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import router from "./router";
